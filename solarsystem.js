371e97d5cbc3661ecf1d8e09092cb637f518928f
David Grayson <dkgrayson@gmail.com>
2023-11-16 18:37:58 -0500

    refactor: Dry up code

--- solarsystem.js
+++ solarsystem.js
@@ -1,6 +1,32 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
+// Objects
+class CelestialBody extends THREE.Mesh {
+    constructor(radius, widthSegments, heightSegments, materialConfig, orbitSpeed) {
+        const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        const material = new THREE.MeshPhongMaterial(materialConfig);
+        super(geometry, material);
+    }
+}
+
+class Sun extends THREE.Mesh {
+    constructor(radius, widthSegments, heightSegments, materialConfig) {
+        const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+        const material = new THREE.MeshBasicMaterial(materialConfig);
+        super(geometry, material);
+        this.rotation.x = Math.PI / 2;
+    }
+}
+
+class CelestialRing extends THREE.Mesh {
+    constructor(radius, radialSegments , tubularSegments, arc, materialConfig) {
+        const geometry = new THREE.TorusGeometry(radius, radialSegments, tubularSegments, arc);
+        const material = new THREE.MeshBasicMaterial(materialConfig);
+        super(geometry, material);
+    }
+}
+
 // Scene 
 const scene = new THREE.Scene();
 
@@ -11,60 +37,69 @@ camera.position.set(15, 15, 30);
 // Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
-
-// Sun
-const sunGeometry = new THREE.SphereGeometry(1.5, 32, 32);
-const sunMaterial = new THREE.MeshBasicMaterial({ color: 0xFFFF00 });
-const sun = new THREE.Mesh(sunGeometry, sunMaterial);
-scene.add(sun);
-
-// Earth
-const earthGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-const earthMaterial = new THREE.MeshPhongMaterial({ color: 0x9090FF, shininess: 50, specular: 0x222222 });
-const earth = new THREE.Mesh(earthGeometry, earthMaterial);
-const earthOrbitSpeed = 0.001;
-scene.add(earth);
-
-// Earth Orbit Ring
-const earthOrbitGeometry = new THREE.TorusGeometry(5, 0.01, 16, 100);
-const earthOrbitMaterial = new THREE.MeshBasicMaterial({ color: 0x888888 });
-const earthOrbit = new THREE.Mesh(earthOrbitGeometry, earthOrbitMaterial);
-earthOrbit.rotation.x = Math.PI / 2;
-scene.add(earthOrbit);
-
-// Moon
-const moonGeometry = new THREE.SphereGeometry(0.1, 32, 32);
-const moonMaterial = new THREE.MeshPhongMaterial({ color: 0xAAAAAA, shininess: 10, specular: 0x111111 });
-const moon = new THREE.Mesh(moonGeometry, moonMaterial);
-const moonOrbitSpeed = 0.002;
-scene.add(moon);
-
-// Moon Orbit Ring (relative to Earth's position)
-const moonOrbitGeometry = new THREE.TorusGeometry(0.7, 0.005, 16, 100);
-const moonOrbitMaterial = new THREE.MeshBasicMaterial({ color: 0x555555 });
-const moonOrbit = new THREE.Mesh(moonOrbitGeometry, moonOrbitMaterial);
-moonOrbit.rotation.x = Math.PI / 2;
-earth.add(moonOrbit);
-
-// Sunlight
-const sunlight = new THREE.PointLight(0xffffff, 1.5, 500);
-sunlight.position.set(0, 0, 0);
-scene.add(sunlight);
-
-// Shadows
-renderer.shadowMap.enabled = true;
-sunlight.castShadow = true;
-earth.castShadow = true;
-earth.receiveShadow = true;
-moon.castShadow = true;
-moon.receiveShadow = true;
-
-// General controls
+// Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
+// Celestial Objects and setup
+const sun = new Sun(
+    1.5,
+    32,
+    32,
+    {color: 0xFFFF00 }
+);
 
+scene.add(sun);
+
+const earth = new CelestialBody(
+    0.5,
+    32,
+    32,
+    { color: 0x9090FF, shininess: 50, specular: 0x222222 }
+);
+
+scene.add(earth);
+
+const moon = new CelestialBody(
+    0.1,
+    32,
+    32,
+    { color: 0xAAAAAA, shininess: 10, specular: 0x111111 }
+);
+
+scene.add(moon);
+
+const earthOrbitSpeed = 0.001;
+const earthOrbit = new CelestialRing(
+    5,
+    0.01,
+    16,
+    100,
+    { color: 0x888888 }
+);
+
+const moonOrbitSpeed = 0.002;
+const moonOrbit = new CelestialRing(
+    0.7,
+    0.0105,
+    16,
+    100,
+    { color: 0x888888 }
+);
+
+moonOrbit.rotation.x = earthOrbit.rotation.x = Math.PI / 2;
+scene.add(earthOrbit);
+scene.add(moonOrbit);
+earth.add(moonOrbit);
+
+const sunlight = new THREE.PointLight(0xffffff, 1.5, 500);
+sunlight.position.set(0, 0, 0);
+sunlight.castShadow = true;
+scene.add(sunlight);
+
+// Main loop
 function animate() {
     requestAnimationFrame(animate);
     updateOrbit(earth, sun, 5, earthOrbitSpeed);
