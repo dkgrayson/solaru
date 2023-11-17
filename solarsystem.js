64ca2d531962e7f3e387ab324a16c4d17a6018fe
David Grayson <dkgrayson@gmail.com>
2023-11-16 19:25:21 -0500

    fix: Sun size

--- solarsystem.js
+++ solarsystem.js
@@ -32,7 +32,7 @@ const scene = new THREE.Scene();
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(15, 15, 30);
+camera.position.set(45, 45, 90);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer();
@@ -47,10 +47,10 @@ const controls = new OrbitControls(camera, renderer.domElement);
 // Celestial Objects and setup
 
 const sun = new Sun(
-    1.5,
+    4.5,
     32,
     32,
-    {color: 0xFFFF00 }
+    { color: 0xFFFF00 }
 );
 scene.add(sun);
 
@@ -61,21 +61,21 @@ function createPlanet(radius, color, distanceFromSun, orbitSpeed) {
 
     const orbitRing = new CelestialRing(distanceFromSun, 0.01, 16, 100, { color: 0x000000 });
     orbitRing.rotation.x = Math.PI / 2;
-    scene.add(orbitRing)
+    scene.add(orbitRing);
+
     return planet;
 }
 
-// Create planets
-const mercury = createPlanet(0.5, 0x909090, 10, 0.004);
-const venus = createPlanet(1.2, 0xa0522d, 18, 0.003);
-const earth = createPlanet(1.3, 0x9090FF, 25, 0.002);
-const mars = createPlanet(0.7, 0xb22222, 35, 0.0018);
-const jupiter = createPlanet(3, 0xf4a460, 55, 0.001);
-const saturn = createPlanet(2.5, 0xdaa520, 70, 0.0009);
-const uranus = createPlanet(2, 0xadff2f, 85, 0.0004);
-const neptune = createPlanet(1.9, 0x4169e1, 100, 0.0001);
 
-// Add planets to the scene
+const mercury = createPlanet(0.5, 0x909090, 12, 0.004);
+const venus = createPlanet(1.2, 0xa0522d, 20, 0.003);
+const earth = createPlanet(1.3, 0x9090FF, 28, 0.002);
+const mars = createPlanet(0.7, 0xb22222, 38, 0.0018);
+const jupiter = createPlanet(3, 0xf4a460, 60, 0.001);
+const saturn = createPlanet(2.5, 0xdaa520, 78, 0.0009);
+const uranus = createPlanet(2, 0xadff2f, 96, 0.0004);
+const neptune = createPlanet(1.9, 0x4169e1, 115, 0.0001);
+
 const planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
 planets.forEach(planet => scene.add(planet));
 
@@ -98,11 +98,6 @@ function updatePlanets() {
         planet.position.x = Math.cos(Date.now() * orbitSpeed) * distanceFromSun;
         planet.position.z = Math.sin(Date.now() * orbitSpeed) * distanceFromSun;
     });
-}
-
-function updateOrbit(object, center, orbitRadius, orbitSpeed) {
-    object.position.x = center.position.x + Math.cos(Date.now() * orbitSpeed) * orbitRadius;
-    object.position.z = center.position.z + Math.sin(Date.now() * orbitSpeed) * orbitRadius;
 }
 
 animate();
