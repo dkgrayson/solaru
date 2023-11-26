4b9f0d2887f07b7d86c597fd0a2c20a76dad6ecb
David Grayson <dkgrayson@gmail.com>
2023-11-26 10:06:55 -0500

    fix: Solar system

--- README.md
+++ README.md
@@ -4,8 +4,14 @@
 
 - Node.js: Latest
 
-## Starting the application
+## Starting the solar system application
 
 1. `npm install`
-2. `npm start`
+2. `npm run solar`
+3. Find the project at `localhost:1234`
+
+## Starting the globe trekker application
+
+1. `npm install`
+2. `npm run trek`
 3. Find the project at `localhost:1234`
--- javascript/solar-system/app.js
+++ javascript/solar-system/app.js
@@ -1,7 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import gltf from './coastal.gltf';
 
 // Objects
 class CelestialBody extends THREE.Mesh {
@@ -34,7 +32,7 @@ const scene = new THREE.Scene();
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(45, 45, 90);
+camera.position.set(15, 15, 30);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer();
@@ -46,52 +44,57 @@ renderer.setClearColor(0xFFFFFF);
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
-const loader = new GLTFLoader();
-
 // Celestial Objects and setup
-let planets = [];
+
 const sun = new Sun(
-    4.5,
+    1.5,
     32,
     32,
-    { color: 0xFFFF00 }
+    {color: 0xFFFF00 }
 );
 scene.add(sun);
 
 function createPlanet(radius, color, distanceFromSun, orbitSpeed) {
-    const materialConfig = { color: color};
+    const materialConfig = { color: color, shininess: 50, specular: 0x222222 };
     const planet = new CelestialBody(radius, 32, 32, materialConfig);
     planet.userData = { distanceFromSun, orbitSpeed };
-
-    const orbitRing = new CelestialRing(distanceFromSun, 0.01, 16, 100, { color: 0x000000 });
-    orbitRing.rotation.x = Math.PI / 2;
-    scene.add(orbitRing);
-
     return planet;
 }
 
-function loadPlanet(gltfPath, distanceFromSun, orbitSpeed) {
-    loader.load(gltfPath, (gltf) => {
-        const planet = gltf.scene;
-        planet.userData = { distanceFromSun, orbitSpeed };
+// Create planets
+const mercury = createPlanet(0.5, 0x909090, 10, 0.004);
+const venus = createPlanet(1.2, 0xa0522d, 18, 0.003);
+const earth = createPlanet(1.3, 0x9090FF, 25, 0.002);
+const mars = createPlanet(0.7, 0xb22222, 35, 0.0018);
+const jupiter = createPlanet(3, 0xf4a460, 55, 0.001);
+const saturn = createPlanet(2.5, 0xdaa520, 70, 0.0009);
+const uranus = createPlanet(2, 0xadff2f, 85, 0.0004);
+const neptune = createPlanet(1.9, 0x4169e1, 100, 0.0001);
 
-        planet.scale.set(0.01, 0.01, 0.01);
+// Add planets to the scene
+const planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
+planets.forEach(planet => scene.add(planet));
 
-        planets.push(planet);
-        scene.add(planet);
-    }, undefined, (error) => {
-        console.error(error);
-    });
-}
+const earthOrbit = new CelestialRing(
+    5,
+    0.01,
+    16,
+    100,
+    { color: 0x888888 }
+);
 
-loadPlanet('./coastal.gltfcdcd s', 12, 0.004);
-// loadPlanet(gltf, 20, 0.003);
-// loadPlanet(gltf, 28, 0.002);
-// loadPlanet(gltf, 38, 0.0018);
-// loadPlanet(gltf, 60, 0.001);
-// loadPlanet(gltf, 78, 0.0009);
-// loadPlanet(gltf, 96, 0.0004);
-// loadPlanet(gltf, 115, 0.0001);
+const moonOrbit = new CelestialRing(
+    0.7,
+    0.0105,
+    16,
+    100,
+    { color: 0x888888 }
+);
+
+moonOrbit.rotation.x = earthOrbit.rotation.x = Math.PI / 2;
+scene.add(earthOrbit);
+scene.add(moonOrbit);
+earth.add(moonOrbit);
 
 const sunlight = new THREE.PointLight(0xffffff, 1.5, 500);
 sunlight.position.set(0, 0, 0);
@@ -112,6 +115,11 @@ function updatePlanets() {
         planet.position.x = Math.cos(Date.now() * orbitSpeed) * distanceFromSun;
         planet.position.z = Math.sin(Date.now() * orbitSpeed) * distanceFromSun;
     });
+}
+
+function updateOrbit(object, center, orbitRadius, orbitSpeed) {
+    object.position.x = center.position.x + Math.cos(Date.now() * orbitSpeed) * orbitRadius;
+    object.position.z = center.position.z + Math.sin(Date.now() * orbitSpeed) * orbitRadius;
 }
 
 animate();
